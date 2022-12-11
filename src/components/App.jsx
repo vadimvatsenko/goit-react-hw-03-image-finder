f26a587d32341361afac5b885270007ecddad142
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2022-12-11 11:51:45 +0200

    11.12.2022 11:51

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,23 @@ import React, {Component} from "react";
 
 import Searchbar from './Searchbar';
 import SearchBarButton from "./SearchBarButton";
-
-import { ReactComponent as SearchIcon } from '../icons/search.svg';
-// импорт иконки как компонента
-
 import ImageGallery from './ImageGallery';
 import ImageGalleryItem from "./ImageGalleryItem";
 import Modal from "./Modal";
+import Loader from "./Loader";
 
+import { ToastContainer } from 'react-toastify';
+
+import { ReactComponent as SearchIcon } from '../icons/search.svg';
+// импорт иконки как компонента
 import axios from "axios";
-
 axios.defaults.baseURL = "https://pixabay.com/api/"
 
 
 export default class App extends Component {
   state = {
     openModal: false,
+    imgName: '',
 
     articles: [],
     isLoading: false,
@@ -26,7 +27,7 @@ export default class App extends Component {
 
   async componentDidMount() {
     this.setState({ isLoading: true });
-
+    
     try {
       const response = await axios.get("?key=29703536-3492bea623abb7896113a32cf&q=yellow+flowers&image_type=photo");
       this.setState({ articles: response.data.hits });
@@ -45,16 +46,22 @@ export default class App extends Component {
     }))
   };
 
+  handleFormSubmit = imgName => {
+    this.setState({imgName: imgName})
+  }
+
   render() {
-    const {openModal, articles} = this.state;
+    const {openModal, articles, isLoading} = this.state;
     return (
       <div>
-        <Searchbar>
+        <Searchbar onSubmit={this.handleFormSubmit}>
           <SearchBarButton aria-label={'search button'}>
               <SearchIcon/>
           </SearchBarButton>
         </Searchbar>
+        <ToastContainer/>
         
+        {isLoading && <Loader/>}
         <ImageGallery>
           <ImageGalleryItem articles={articles } />
         </ImageGallery>
@@ -64,7 +71,7 @@ export default class App extends Component {
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium corrupti eum voluptates voluptatum animi totam voluptatem adipisci, ipsa dignissimos dolor! Beatae deserunt adipisci, itaque iure provident vero expedita modi? At?</p>
           <button type='button' onClick={this.toggleModal}>Close</button>
         </Modal> }
-        
+
 
 
       </div>
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,12 +1,37 @@
 import style from '../Searchbar/Searchbar.module.scss';
 import React, { Component } from "react";
+import { toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 // import SearchFormButton from '../SearchBarButton/seachBarButton';
 
 export default class Searchbar extends Component {
+    state = {
+        imgName: ''
+    }
+
+    handleChangeName = e => {
+        this.setState({ imgName: e.currentTarget.value.toLowerCase() })
+    };
+    handleSubmit = e => {
+        e.preventDefault();
+        if (this.state.imgName.trim() === '') {
+            toast.error('Empty search', {
+                autoClose: 1000
+            });
+            return;
+        }
+        this.props.onSubmit(this.state.imgName);
+        //проп с APP в него передаем значение submit
+        this.setState({ imgName: '' })
+        //очистка после submit
+    };
+
     render() { 
         return (
             <header className={style.searchbar}>
-                <form className={style.searchForm}>
+                <form
+                    className={style.searchForm}
+                    onSubmit={this.handleSubmit}>
 
                     {this.props.children}
 
@@ -16,6 +41,7 @@ export default class Searchbar extends Component {
                         autoComplete="off"
                         autoFocus
                         placeholder="Search images and photos"
+                        onChange={this.handleChangeName}
                     />
                 </form>
             </header>
