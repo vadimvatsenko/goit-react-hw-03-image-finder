1a51c4cc5a8f1769374913316e72f930cd418511
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2022-12-12 12:01:34 +0200

    12.12.2022 12:01

--- src/components/Gallery/Gallary.js
+++ src/components/Gallery/Gallary.js
@@ -1,18 +1,21 @@
 import { Component } from "react";
 import Loader from "components/Loader";
 
-import axios from "axios";
+// import axios from "axios";
 
 
 import ImageGallery from "components/ImageGallery";
 import ImageGalleryItem from "components/ImageGalleryItem";
 import Error from "components/Error";
 import Empty from "components/Empty";
+import Api from "components/services/API";
 
-axios.defaults.baseURL = "https://pixabay.com/api/";
-const API_KEY = '29703536-3492bea623abb7896113a32cf';
-const page = 1;
-const perPage = 12;
+// axios.defaults.baseURL = "https://pixabay.com/api/";
+// const API_KEY = '29703536-3492bea623abb7896113a32cf';
+// const page = 1;
+// const perPage = 12;
+
+const newApiService = new Api();
 
 export default class Gallery extends Component {
     state = {
@@ -22,14 +25,31 @@ export default class Gallery extends Component {
         status: 'idle'
     }
 
+    //  async componentDidUpdate(prevProps) {
+    //     const prevName = prevProps.imgName;
+    //     const nextName = this.props.imgName
+    //     if (prevName !== nextName) {
+    //         this.setState({ status: 'pandings' });
+    //         try {
+    //         const response = await axios.get(`?key=${API_KEY}&q=${nextName}&page=${page}&image_type=photo&orientation=horizontal&per_page=${perPage}`);
+    //         this.setState({ imageList: response.data.hits, status: 'resolved' });
+    //     } catch (error) {
+    //         this.setState({ error, status: 'rejected' });
+    //     } finally {
+    //         this.setState({ isLoading: false });
+    //     }
+
+    //     }
+    // };
+
      async componentDidUpdate(prevProps) {
         const prevName = prevProps.imgName;
         const nextName = this.props.imgName
         if (prevName !== nextName) {
             this.setState({ status: 'pandings' });
             try {
-            const response = await axios.get(`?key=${API_KEY}&q=${nextName}&page=${page}&image_type=photo&orientation=horizontal&per_page=${perPage}`);
-            this.setState({ imageList: response.data.hits, status: 'resolved' });
+                const imgObj = await newApiService.fetchImg(nextName);
+            this.setState({ imageList: imgObj, status: 'resolved' });
         } catch (error) {
             this.setState({ error, status: 'rejected' });
         } finally {
