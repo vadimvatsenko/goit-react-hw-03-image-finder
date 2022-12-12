f079c30e6e1624742ac288cc564554020dacf3c0
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2022-12-12 12:16:34 +0200

    12.12.2022 12:16

--- src/components/Gallery/Gallary.js
+++ src/components/Gallery/Gallary.js
@@ -1,60 +1,34 @@
 import { Component } from "react";
 import Loader from "components/Loader";
 
-// import axios from "axios";
-
-
 import ImageGallery from "components/ImageGallery";
 import ImageGalleryItem from "components/ImageGalleryItem";
 import Error from "components/Error";
 import Empty from "components/Empty";
 import Api from "components/services/API";
 
-// axios.defaults.baseURL = "https://pixabay.com/api/";
-// const API_KEY = '29703536-3492bea623abb7896113a32cf';
-// const page = 1;
-// const perPage = 12;
-
 const newApiService = new Api();
 
 export default class Gallery extends Component {
     state = {
-
         imageList: null,
         error: null,
         status: 'idle'
     }
 
-    //  async componentDidUpdate(prevProps) {
-    //     const prevName = prevProps.imgName;
-    //     const nextName = this.props.imgName
-    //     if (prevName !== nextName) {
-    //         this.setState({ status: 'pandings' });
-    //         try {
-    //         const response = await axios.get(`?key=${API_KEY}&q=${nextName}&page=${page}&image_type=photo&orientation=horizontal&per_page=${perPage}`);
-    //         this.setState({ imageList: response.data.hits, status: 'resolved' });
-    //     } catch (error) {
-    //         this.setState({ error, status: 'rejected' });
-    //     } finally {
-    //         this.setState({ isLoading: false });
-    //     }
-
-    //     }
-    // };
-
-     async componentDidUpdate(prevProps) {
+    async componentDidUpdate(prevProps) {
         const prevName = prevProps.imgName;
         const nextName = this.props.imgName
-        if (prevName !== nextName) {
-            this.setState({ status: 'pandings' });
-            try {
+            if (prevName !== nextName) {
+                this.setState({ status: 'pandings' });
+                try {
                 const imgObj = await newApiService.fetchImg(nextName);
-            this.setState({ imageList: imgObj, status: 'resolved' });
-        } catch (error) {
-            this.setState({ error, status: 'rejected' });
-        } finally {
-            this.setState({ isLoading: false });
-        }
+                this.setState({ imageList: imgObj, status: 'resolved' });
+            } catch (error) {
+                this.setState({ error, status: 'rejected' });
+            } finally {
+                this.setState({ isLoading: false });
+            }
 
         }
     };
--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,9 +1,5 @@
 import React from "react";
 import style from './ImageGalleryItem.module.scss';
-// import { nanoid } from 'nanoid';
-
-// const imageGalleryKey = nanoid();
-
 
 
 export default function ImageGalleryItem({ imageList }) {
@@ -11,9 +7,16 @@ export default function ImageGalleryItem({ imageList }) {
     return (
         <>
             
-       {imageList.map(({ id, webformatURL, tags }) => ( 
-        <li key={ id} className={style.ImageGalleryItem}>
-            <img className={style.ImageGalleryItemImage} src={webformatURL} alt={tags} />
+       {imageList.map(({ id, webformatURL, tags, largeImageURL }) => ( 
+           <li
+               key={id}
+               className={style.ImageGalleryItem}>
+               <img
+                   className={style.ImageGalleryItemImage}
+                   src={webformatURL}
+                   alt={tags}
+                   data-src={largeImageURL}
+                   onClick={ } />
             
            </li>
            ))}
